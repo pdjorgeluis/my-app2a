b30b1afebcf9bd40c021fedf4e72f14c73814222
Jorge Luis Prieto Diaz <pdjorgeluis@gmail.com>
2024-05-24 10:48:52 -0400

    Added axios to fetch Notes from JSON server

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios'
+
+/*const promise = axios.get('http://localhost:3001/notes');
+promise.then(response => {
+  console.log(response)
+})
+
+
+const promise2 = axios.get('http/localhost:3001/foobar');
+console.log(promise2);*/
 
 const notes = [
   {
